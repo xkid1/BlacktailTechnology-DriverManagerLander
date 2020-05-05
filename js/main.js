df93875c39fc238b8819a009856b7faa63972531
vkings1 <elmer.alluad@gmail.com>
2020-05-06 00:48:37 +0800

    new text file added

---
+++ js/main.js
@@ -0,0 +1,7 @@
+const scanForFree = document.getElementById('scanForFree');
+
+scanForFree.addEventListener('click', ()=> {
+    document.getElementById('bodyBlock').style.display="block";
+    document.getElementById('windowInstruction').style.display="block";
+
+});
